5ff652b0598c2df1c8859a54bfd2a08e89c4d3c2
Bhavna Shende <bhavanashende661@gmail.com>
2022-03-21 03:16:58 -0400

    Added: Key Framework Project Testing

---
+++ src/test/java/com/qa/hs/keyword/test/LoginTest.java
@@ -0,0 +1,17 @@
+package com.qa.hs.keyword.test;
+
+import com.qa.hs.keyword.engine.KeyWordEngine;
+import org.testng.annotations.Test;
+
+    public class LoginTest {
+
+        public KeyWordEngine keyWordEngine;
+        @Test
+
+        public void loinTest() throws InterruptedException {
+            keyWordEngine= new KeyWordEngine();
+            keyWordEngine.startExecution("login");
+            Thread.sleep(3000);
+        }
+    }
+
